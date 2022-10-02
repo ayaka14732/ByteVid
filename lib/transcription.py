ea76a2c19d3eab8382cf7b3b8ea5cb91192b783a
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-10-03 00:12:23 +0800

    fix bug

--- lib/transcription.py
+++ lib/transcription.py
@@ -6,4 +6,4 @@ def transcribe(work_dir: str, filename_preprocessed: str, language: Optional[str
     language_option = () if not language else ('--language', language)
     device_option = () if not device else ('--device', device)
     file_in = os.path.join(work_dir, filename_preprocessed)
-    subprocess.run(('whisper', *device_option, '--task', 'transcribe', *language_option, '--output_dir', work_dir, file_in))
+    subprocess.run(('whisper', *device_option, '--task', 'transcribe', *language_option, '--output_dir', work_dir, file_in), check=True)
--- lib/video_preprocessing.py
+++ lib/video_preprocessing.py
@@ -8,6 +8,6 @@ def speedup_1_6x(work_dir: str) -> str:
 
     _, filename_preprocessed = os.path.split(file_out)
 
-    subprocess.run(('ffmpeg', '-y', '-i', file_in, '-filter_complex', '[0:v]setpts=0.625*PTS[v];[0:a]atempo=1.6[a]', '-map', '[v]', '-map', '[a]', file_out))
+    subprocess.run(('ffmpeg', '-y', '-i', file_in, '-filter_complex', '[0:v]setpts=0.625*PTS[v];[0:a]atempo=1.6[a]', '-map', '[v]', '-map', '[a]', file_out), check=True)
 
     return filename_preprocessed
